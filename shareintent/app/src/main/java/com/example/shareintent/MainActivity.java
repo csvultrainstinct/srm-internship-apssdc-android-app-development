8830f1924049ba71cd437bf87f9f705e35855eb8
vibhav <vibhavchirravuri996@gmail.com>
2020-06-30 21:39:26 +0530

    shareintent

---
+++ shareintent/app/src/main/java/com/example/shareintent/MainActivity.java
@@ -0,0 +1,31 @@
+package com.example.shareintent;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+EditText et;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        et=findViewById(R.id.editTextTextPersonName);
+    }
+
+
+    public void send(View view) {
+        Intent sendIntent = new Intent();
+        sendIntent.setAction(Intent.ACTION_SEND);
+        sendIntent.putExtra(Intent.EXTRA_TEXT, "This is my text to send.");
+        sendIntent.setType("text/plain");
+        Intent shareIntent = Intent.createChooser(sendIntent, null);
+        startActivity(shareIntent);
+    }
+
+}
